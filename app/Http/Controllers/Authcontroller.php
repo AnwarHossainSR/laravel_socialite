f19e81271548389ddefcd77ea281a00c7012c986
Anwar Hossain <mahedianwar@gmail.com>
2022-11-29 10:13:36 +0600

    implement sanctum

---
+++ app/Http/Controllers/Authcontroller.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class Authcontroller extends Controller
+{
+    public function register(Request $request)
+    {
+        $fields = $request->validate([
+            "name" => 'required|string',
+            "email" => 'required|string|unique:users,email',
+            "password" => 'required|string|confirmed'
+        ]);
+
+        $user = User::create([
+            'name' => $fields['name'],
+            'email' => $fields['email'],
+            'password' => bcrypt($fields['password']),
+        ]);
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token,
+        ];
+
+        return response($response, 201);
+    }
+
+    public function login(Request $request)
+    {
+        $fields = $request->validate([
+            "email" => 'required|string',
+            "password" => 'required|string',
+        ]);
+
+        // Check Email
+        $where = ["email" => $fields['email']];
+        $user = User::where($where)->first();
+
+        // Check Password
+        if (!$user || !Hash::check($fields['password'], $user->password)) {
+            return response([
+                'message' => "Bad credentials"
+            ], 401);
+        }
+
+        $token = $user->createToken('token')->plainTextToken;
+
+        $response = [
+            'user' => $user,
+            'token' => $token
+        ];
+
+        return response($response, 201);
+    }
+
+    public function logout(Request $request)
+    {
+        auth()->user()->tokens()->delete();
+
+        $response = [
+            'message' => 'logged out'
+        ];
+
+        return response($response, 201);
+    }
+}
